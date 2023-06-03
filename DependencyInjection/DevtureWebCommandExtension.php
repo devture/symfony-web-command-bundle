9b7974f4200ba112b361f914ae9c28eee5bc038e
Slavi Pantaleev <slavi@devture.com>
2023-06-03 16:14:53 +0300

    Add some typehints

--- DependencyInjection/DevtureWebCommandExtension.php
+++ DependencyInjection/DevtureWebCommandExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\Loader;
 
 class DevtureWebCommandExtension extends Extension {
 
-	public function load(array $configs, ContainerBuilder $container) {
+	public function load(array $configs, ContainerBuilder $container): void {
 		$configuration = new Configuration();
 		$config = $this->processConfiguration($configuration, $configs);
 
