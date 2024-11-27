05facdb076d23cb82a19d6fec395bd77ade73812
Slavi Pantaleev <slavi@devture.com>
2024-11-27 14:48:40 +0200

    Extend Extension from symfony/dependency-injection, not from symfony/http-kernel

--- DependencyInjection/DevtureWebCommandExtension.php
+++ DependencyInjection/DevtureWebCommandExtension.php
@@ -1,9 +1,9 @@
 <?php
 namespace Devture\Bundle\WebCommandBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 class DevtureWebCommandExtension extends Extension {
